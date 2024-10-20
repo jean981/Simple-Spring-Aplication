f1b9913e78f280d2d2ec0d99050a7e13d9574ce9
com.jps.jeanpa <jean.sa@estudante.iftm.edu.br>
2024-10-20 03:11:54 +0100

    🔧 chore(): updated imports javax.* to jakarta.*

--- src/main/java/com/appjavaweb/appspring/entities/Category.java
+++ src/main/java/com/appjavaweb/appspring/entities/Category.java
@@ -1,17 +1,12 @@
 package com.appjavaweb.appspring.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Category implements Serializable {
--- src/main/java/com/appjavaweb/appspring/entities/Product.java
+++ src/main/java/com/appjavaweb/appspring/entities/Product.java
@@ -1,14 +1,10 @@
 package com.appjavaweb.appspring.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 
 @Entity
 public class Product implements Serializable {
